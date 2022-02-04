8e1eaacd1f45974010fa6c29ce8c788bbad54faf
zainir youssouf <zainir youssouf@st.com>
2022-02-04 12:10:37 +0100

    rest api

---
+++ rest-api-server/nodejs-server-generated/controllers/usersController.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var varusersController = require('./usersControllerService');
+
+module.exports.funcusersGET = function funcusersGET(req, res, next) {
+  varusersController.funcusersGET(req.swagger.params, res, next);
+};
---
+++ rest-api-server/nodejs-server-generated/controllers/usersControllerService.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports.funcusersGET = function funcusersGET(req, res, next) {
+  res.send({
+    message: 'This is the mockup controller for funcusersGET'
+  });
+};
